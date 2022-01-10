3cb64d206e3e720ce37adef3ddff91194dcca87a
Rutger Cappendijk <rutgercappendijk@gmail.com>
2022-01-10 09:52:20 +0100

    tokenizer update

--- unit-tests/lexing/lexing_tester.c
+++ unit-tests/lexing/lexing_tester.c
@@ -1,25 +1,28 @@
+#include <string.h>
 #include "headers.h"
 
-static void	write_type(t_type type)
+static void	write_type(t_type type, int mode)
 {
+	if (mode > 0)
+		ft_putstr_fd("type: ", 1);
 	switch (type)
 	{
-	case word:
+	case WORD:
 		ft_putendl_fd("word", 1);
 		break;
-	case e_pipe:
+	case PIPE:
 		ft_putendl_fd("pipe", 1);
 		break;
-	case red_oput:
+	case RED_OPUT:
 		ft_putendl_fd("red_oput", 1);
 		break;
-	case red_iput:
+	case RED_IPUT:
 		ft_putendl_fd("red_iput", 1);
 		break;
-	case red_oput_a:
+	case RED_OPUT_A:
 		ft_putendl_fd("red_iput_a", 1);
 		break;
-	case red_iput_a:
+	case RED_IPUT_A:
 		ft_putendl_fd("red_iput_a", 1);
 		break;
 	default:
@@ -28,38 +31,74 @@ static void	write_type(t_type type)
 	}
 }
 
-static void	write_token(t_token *token)
+static void	write_token(t_token *token, int mode)
 {
+	if (mode > 0)
+		ft_putstr_fd("text: ", 1);
 	if (!token->text)
 		ft_putendl_fd("null", 1);
 	else
 		ft_putendl_fd(token->text, 1);
-	ft_putnbr_fd((int)token->len, 1);
-	ft_putchar_fd('\n', 1);
-	write_type(token->type);
+	if (mode > 0)
+	{
+		ft_putstr_fd("len: ", 1);
+		ft_putnbr_fd((int)token->len, 1);
+		ft_putchar_fd('\n', 1);
+	}
+	write_type(token->type, mode);
 }
 
-static void	write_all_tokens(t_token *tokens)
+static void	write_all_tokens(t_token *tokens, int mode)
 {
 	int		i = 0;
 	t_token	*curr;
 
 	curr = tokens;
-	while (tokens != NULL)
+	while (curr->type != TOKEN_EOF)
 	{
-		ft_putnbr_fd(i, 1);
-		ft_putchar_fd('\n', 1);
-		write_token(tokens);
+		if (mode > 1)
+		{
+			ft_putstr_fd("i: ", 1);
+			ft_putnbr_fd(i, 1);
+			ft_putchar_fd('\n', 1);
+		}
+		write_token(curr, mode);
 		curr = curr->next;
+		if (curr->type != TOKEN_EOF)
+			ft_putchar_fd('\n', 1);
 		i++;
 	}
-	ft_putendl_fd("Done", 1);
+	ft_putendl_fd("----------------", 1);
 }
 
-int main(void)
+static void	do_test(char *line, int mode)
 {
 	t_token	*tokens;
 
-	tokens = tokenize_cmd("ls -l");
-	write_all_tokens(tokens);
+	tokens = tokenize_cmd(line);
+	write_all_tokens(tokens, mode);
+	free_token_list(&tokens);
+}
+
+
+/*
+	add tests here.
+*/
+static void	auto_tests(void)
+{
+	do_test("ls -l", 0);
+	do_test("hi", 0);
+	do_test("blabla", 0);
+	do_test("sleep 3", 0);
+	do_test("sleep a", 0);
+	do_test("   sleep   ", 0);
+	do_test("sleep 3 | sleep 3", 0);
+}
+
+int main(int argc, char **argv)
+{	
+	if (argc > 1)
+		do_test(argv[1], argc);
+	else
+		auto_tests();
 }
