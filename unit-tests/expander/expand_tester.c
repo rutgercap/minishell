49867ef1eeb3533ed1aae842483b9350ae9c7976
Daan Van der plas <dvan-der@f1r1s11.codam.nl>
2022-01-27 11:55:21 +0100

    parser merge

--- unit-tests/expander/expand_tester.c
+++ unit-tests/expander/expand_tester.c
@@ -14,16 +14,24 @@ int main(int argc, char **argv, char **env)
 {
 	(void)argc;
 	(void)argv;
-	test("", 0, env);
-	test("nothing \"should happen\" here", 0, env);
-	test("expand $USER", 0, env);
-	test("$", 0, env);
-	test("$ $ $ $", 0, env);
-	test("echo \"hi\"", 0, env);
-	test("$USER", 0, env);
-	test("\'$USER\'", 0, env);
-	test("$?hi", 0, env);
-	test("$?$USER", 0, env);
-	test("$?$USERhi", 0, env);
-	test("$?$USER$?", 0, env);
+	// test("", 0, env);
+	// test("nothing \"should happen\" here", 0, env);
+	// test("expand $USER", 0, env);
+	test("HOME=$HOME:hallo", 0, env);	// these three should append hallo to $HOME
+	test("HOME='$HOME'hallo", 0, env);	// these three should append hallo to $HOME
+	test("HOME=$HOME/hallo", 0, env);
+	test("HOME=$HOME%hallo", 0, env);
+	test("HOME=$HOME|hallo", 0, env);	// error, hello not found
+	test("HOME=$HOME'hallo", 0, env);	// error, searching for second '
+	test("HOME=$HOME>hallo", 0, env);
+	test("HOME=$HOME<hallo", 0, env);
+	// test("$", 0, env);
+	// test("$ $ $ $", 0, env);
+	// test("echo \"hi\"", 0, env);
+	// test("$USER", 0, env);
+	// test("\'$USER\'", 0, env);
+	// test("$?hi", 0, env);
+	// test("$?$USER", 0, env);
+	// test("$?$USERhi", 0, env);
+	// test("$?$USER$?", 0, env);
 }
